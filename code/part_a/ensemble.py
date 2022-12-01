4867a0a41eab7799863594129f5c12a1b4c1da42
Ted <longtaideng@gmail.com>
2022-11-30 20:44:04 -0500

    ensemble.py finished.

--- code/part_a/ensemble.py
+++ code/part_a/ensemble.py
@@ -78,51 +78,68 @@ def main():
     # Train model 1 (also tune hyperparameters based using validation data):
     # KNN_inputed_by_user model:
     k_val = [1, 6, 11, 16, 21, 26]
-    knn_best_k = k_val[0]
-    knn_best_acc = 0_
+    knn_best_k_1 = k_val[0]
+    knn_best_acc_1 = 0
     for k in k_val:
         acc_temp = knn_impute_by_user(train_matrix_1, val_data, k)
-        if (acc_temp > knn_best_acc):
-            knn_best_acc = acc_temp
-            knn_best_k = k
+        if (acc_temp > knn_best_acc_1):
+            knn_best_acc_1 = acc_temp
+            knn_best_k_1 = k
 
     # Train model 2 (also tune hyperparameters based using validation data):
     # IRT model:
     # TODO: Train and find best parameter using <train_matrix_2>!
-
+    knn_best_k_2 = k_val[0]
+    knn_best_acc_2 = 0
+    for k in k_val:
+        acc_temp = knn_impute_by_user(train_matrix_2, val_data, k)
+        if (acc_temp > knn_best_acc_2):
+            knn_best_acc_2 = acc_temp
+            knn_best_k_2 = k
 
 
     # Train model 3 (also tune hyperparameters based using validation data):
     # Neural nets model:
     # TODO: Train and find best parameter using <train_matrix_3>!
 
-
+    knn_best_k_3 = k_val[0]
+    knn_best_acc_3 = 0
+    for k in k_val:
+        acc_temp = knn_impute_by_user(train_matrix_3, val_data, k)
+        if (acc_temp > knn_best_acc_3):
+            knn_best_acc_3 = acc_temp
+            knn_best_k_3 = k
 
     # Average predictions of 3 models on validation and testing data (with tuned hyperparameters) for final results, then report them:
     # Note that technically can tune hyperparameters of three base models at the same time, but then the combinations of hyperparameters are way too many for runtime, so we will just pick the best of each world.
     
     # KNN predict on validation and test set:
-    nbrs = KNNImputer(n_neighbors=knn_best_k)
-    pred_valid_knn = nbrs.fit_transform(train_matrix_1) 
-    pred_test_knn = pred_valid_knn
+    nbrs = KNNImputer(n_neighbors=knn_best_k_1)
+    pred_knn_1 = nbrs.fit_transform(train_matrix_1) 
+
+    nbrs = KNNImputer(n_neighbors=knn_best_k_2)
+    pred_knn_2 = nbrs.fit_transform(train_matrix_2) 
+
+    nbrs = KNNImputer(n_neighbors=knn_best_k_3)
+    pred_knn_3 = nbrs.fit_transform(train_matrix_3) 
+
     
     # IRT predict on validation and test set:
-    pred_valid_irt = ...
-    pred_test_irt = ...
+    #pred_valid_irt = ...
+    #pred_test_irt = ...
 
 
     # Neural nets predict on validation and test set:
-    pred_valid_neural_net = ...
-    pred_test_neural_net = ...
+    #pred_valid_neural_net = ...
+    #pred_test_neural_net = ...
     
 
     # Average final predictions on validation and test set:
-    pred_valid_final = (pred_valid_knn + pred_valid_irt + pred_valid_neural_net) / 3
-    pred_test_final = (pred_test_knn + pred_test_irt + pred_test_neural_net) / 3
+    pred_final = (pred_knn_1 + pred_knn_2 + pred_knn_3) / 3
 
     # Report/print results!
-    acc_valid_final = sparse_matrix_evaluate(val_data, pred_valid_final) 
-    acc_test_final = sparse_matrix_evaluate(test_data, pred_test_final)
+    acc_valid_final = sparse_matrix_evaluate(val_data, pred_final) 
+    acc_test_final = sparse_matrix_evaluate(test_data, pred_final)
     print("Final validation accuracy: " + str(acc_valid_final))
     print("Final test accuracy: " + str(acc_test_final))
 
