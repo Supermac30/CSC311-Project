ebf80678eb403c4e75e114ce1f6ea87dd0b9045d
Ted <longtaideng@gmail.com>
2022-12-05 00:58:40 -0500

    Update on knn.py

--- code/part_a/knn.py
+++ code/part_a/knn.py
@@ -64,10 +64,10 @@ def main():
     val_data = load_valid_csv("../data")
     test_data = load_public_test_csv("../data")
 
-    print("Sparse matrix:")
-    print(sparse_matrix)
-    print("Shape of sparse matrix:")
-    print(sparse_matrix.shape)
+    #print("Sparse matrix:")
+    #print(sparse_matrix)
+    #print("Shape of sparse matrix:")
+    #print(sparse_matrix.shape)
 
     #####################################################################
     # TODO:                                                             #
